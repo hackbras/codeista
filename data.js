97f2149127c42930a096d8da982941cc8e4aab50
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2017-11-21 19:11:47 -0300

    second commit

---
+++ data.js
@@ -0,0 +1,13 @@
+var definition = [{
+    "language": "pt-br",
+    "title": "Codeista",
+    "phone": "(31) 9 9562-6585",
+    "email": "contato@codeista.com",
+    "description": "Desenvolvimento web e mobile"
+}]
+
+var contacts = ["email", "phone", "chat"]
+
+var menu = [{
+    "primary-menu": ""
+}]
