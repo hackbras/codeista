0c1cf4e252dbd4057572cf02f7c18bbfe55d063d
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2017-11-23 19:09:02 -0300

    working with arrays

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
-var ourRequest = new XMLHttpRequest();
+var teste = '{"name":"john","age":"31","city":"New York"}';
+var myObj = JSON.parse(teste);
+document.getElementById("demo").innerHTML = myObj.name;
 
-ourRequest.open('GET', 'pt-br.json');
-ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-};
-ourRequest.send();
+
+// var ourRequest = new XMLHttpRequest();
+
+// ourRequest.open('GET', 'pt-br.json');
+// ourRequest.onload = function() {
+//     var ourData = JSON.parse(ourRequest.responseText);
+//     console.log(ourData[0]);
+// };
+// ourRequest.send();
