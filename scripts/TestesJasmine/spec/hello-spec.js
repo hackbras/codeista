1abc3d52fc9e6fa3218b16dfecabf7a7e4bce17c
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2017-11-27 18:25:08 -0300

    To remove file tests

--- scripts/TestesJasmine/spec/hello-spec.js
+++
@@ -1,7 +0,0 @@
-var hello = require('../hello');
-describe('Hello', function() {
-    it('has to print "Hello World!"', function() {
-        var text = hello();
-        expect(text).toEqual('Hello World!');
-    })
-})
