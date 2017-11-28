1db98bbed9f8739864101d3bbeafafca88e84a05
Miquéias M.Sousa <hackbras@outlook.com>
2017-11-28 18:22:19 -0300

    update Javascript

--- controller/main.js
+++ controller/main.js
@@ -12,15 +12,16 @@ function onload() {
         document.getElementById("content__second-section").innerHTML = ourData[0].test_text;
         document.getElementById("content__third-section").innerHTML = ourData[0].test_text;
         document.getElementById("content__aside").innerHTML = ourData[0].test_text;
-        document.getElementById("header__primary--nav-logo").innerHTML = ourData[1].primary-menu_first;
-        document.getElementById("header__primary--nav-search").innerHTML = ourData[1].primary-menu_second;
-        document.getElementById("header__primary--nav-mode").innerHTML = ourData[1].primary-menu_third;
-        document.getElementById("header__primary--nav-language").innerHTML = ourData[1].primary-menu_fourth;
-
-        document.getElementById("header__secondary--nav-about").innerHTML = ourData[1].secondary-menu_first;
-        document.getElementById("header__secondary--nav-services").innerHTML = ourData[1].secondary-menu_second;
-        document.getElementById("header__secondary--nav-courses").innerHTML = ourData[1].secondary-menu_third;
-        document.getElementById("header__secondary--nav-blog").innerHTML = ourData[1].secondary-menu_fourth;
+        //Menu options
+        document.getElementById("header__primary--nav-logo").innerHTML = ourData[1].PM_first;
+        document.getElementById("header__primary--nav-search").innerHTML = ourData[1].PM_second;
+        document.getElementById("header__primary--nav-mode").innerHTML = ourData[1].PM_third;
+        document.getElementById("header__primary--nav-language").innerHTML = ourData[1].PM_fourth;
+        document.getElementById("header__secondary--nav-about").innerHTML = ourData[1].SM_first;
+        document.getElementById("header__secondary--nav-services").innerHTML = ourData[1].SM_second;
+        document.getElementById("header__secondary--nav-courses").innerHTML = ourData[1].SM_third;
+        document.getElementById("header__secondary--nav-blog").innerHTML = ourData[1].SM_fourth;
+        document.getElementById("header__secondary--nav-faq").innerHTML = ourData[1].SM_fifth;
     };
     xhttp.send();
 }
