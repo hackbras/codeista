32e02d3c91aeca2237f62b2efef0c3b868b4ae9d
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2017-11-28 09:02:10 -0300

    starting to work with ajax and json

--- scripts/main.js
+++ scripts/main.js
@@ -1,155 +1,17 @@
-var teste = '{"name":"john","age":"31","city":"New York"}';
-var myObj = JSON.parse(teste);
-document.getElementById("demo").innerHTML = myObj.name;
-
 function onload() {
-    alert('teste');
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.open("GET", "pt-br.json", true);
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://hackbras.github.io/codeista/data/pt-br.json", true);
 
-    // xhttp.onload = function() {
-    //     var ourData = JSON.parse(xhttp.responseText);
-    //     document.getElementById("lang").innerHTML = ourData[0];
-    // };
-
-    // xhttp.send();
+    xhttp.onload = function() {
+        var ourData = JSON.parse(xhttp.responseText);
+        document.getElementById("dev").innerHTML = ourData[0].developer;
+        document.getElementById("celular").innerHTML = ourData[0].phone;
+        document.getElementById("email").innerHTML = ourData[0].email;
+        document.getElementById("atendimento").innerHTML = ourData[0].business_hours;
+        document.getElementById("content__first-section").innerHTML = ourData[0].test_text;
+        document.getElementById("content__second-section").innerHTML = ourData[0].test_text;
+        document.getElementById("content__third-section").innerHTML = ourData[0].test_text;
+        document.getElementById("content__aside").innerHTML = ourData[0].test_text;
+    };
+    xhttp.send();
 }
-
-// var ourRequest = new XMLHttpRequest();
-
-// ourRequest.open('GET', 'pt-br.json');
-// ourRequest.onload = function() {
-//     var ourData = JSON.parse(ourRequest.responseText);
-//     console.log(ourData[0]);
-// };
-// ourRequest.send();
-
-(function() {
-  var app = angular.module('site', []);
-
-  app.controller('StoreController', function(){
-    this.products = gems;
-  });
-
-  app.controller('TabController', function(){
-    this.tab = 1;
-
-    this.setTab = function(newValue){
-      this.tab = newValue;
-    };
-
-    this.isSet = function(tabName){
-      return this.tab === tabName;
-    };
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
-  var gems = [{
-      name: 'Azurite',
-      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-      shine: 8,
-      price: 110.50,
-      rarity: 7,
-      color: '#CCC',
-      faces: 14,
-      images: [
-        "images/gem-02.gif",
-        "images/gem-05.gif",
-        "images/gem-09.gif"
-      ],
-      reviews: [{
-        stars: 5,
-        body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
-      }]
-    }, {
-      name: 'Bloodstone',
-      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-      shine: 9,
-      price: 22.90,
-      rarity: 6,
-      color: '#EEE',
-      faces: 12,
-      images: [
-        "images/gem-01.gif",
-        "images/gem-03.gif",
-        "images/gem-04.gif",
-      ],
-      reviews: [{
-        stars: 3,
-        body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 4,
-        body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
-      }]
-    }, {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "images/gem-06.gif",
-        "images/gem-07.gif",
-        "images/gem-08.gif"
-      ],
-      reviews: [{
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }]
-    }];
-})();
-
-
-// (function(){
-//     var app = angular.module('site',[]);
-//     app.contatos = componentes;
-
-//     var componentes = [{
-//         name: 'Cabeçalho',
-//         description: "Texto descritivo do site"
-//     },
-//     {
-//         name: 'Página inicial',
-//         description: 'Página inicial'        
-//     },
-//     {
-//         name: 'Rodapé',
-//         description: 'Rodapé do site',
-//         developer: 'codeista',
-//         phone: '(31)9 9562-6585',
-//         email: 'hackbras@outlook.com',
-//         address: 'Rua: Saturno, 346, Los Angeles - ibirité - MG'
-//     }];
-
-// })();
